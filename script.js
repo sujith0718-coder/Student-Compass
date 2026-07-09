c11202879f040df6a3abc21f3d697362cdcf1b52
sujith0718-coder <sujith0718@gmail.com>
2026-07-10 00:11:33 +0530

    only final check

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ if (form) {
 
 // Dashboard page
 
-localStorage.clear();
+//localStorage.clear();
 let currentTask=Number(localStorage.getItem("currentTask"))||0;
 let weeklycurrentTask=Number(localStorage.getItem("weeklycurrentTask"))||0;
 let p=Number(localStorage.getItem("p"))||0;
